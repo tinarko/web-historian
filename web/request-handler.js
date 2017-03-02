7c043ee85c00c51c3edee64ff2919599a3460a7e
tinarko <tinarchan@gmail.com>
2017-03-01 16:12:30 -0800

    Pass readListOfUrls test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,21 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelper = require('./http-helpers.js');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  var asset;
+  if (req.url === '/') {
+    asset = archive.paths.siteAssets + '/index.html';
+  } else {
+    asset = archive.paths.archivedSites + req.url;
+  }
+  console.log(asset);
+
+  httpHelper.serveAssets(res, asset, function(data) {
+    res.end(data);
+  });
+
 };
