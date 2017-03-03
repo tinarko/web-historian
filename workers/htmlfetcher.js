c44e844c26ecb3aa1e08859368daeb541a6d3a3c
tinarko <tinarchan@gmail.com>
2017-03-02 17:00:31 -0800

    downloadUrl saves entire index.html

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,9 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('/Users/student/Desktop/hrsf72-web-historian/helpers/archive-helpers');
+// archive.downloadUrls();
+
+archive.readListOfUrls(function(urls) {
+  console.log('heyyyy', urls);
+  archive.downloadUrls(urls);
+});
